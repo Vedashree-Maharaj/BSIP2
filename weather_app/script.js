9ad584d2a370ce8378ed6e4f455502d40f64b5d7
Vedashree-Maharaj <maharajvedashree2004@gmail.com>
2026-08-08 00:48:43 +0530

    Add files via upload

---
+++ weather_app/script.js
@@ -0,0 +1,27 @@
+function getWeather() {
+    let city = document.getElementById("city").value.trim();
+    let apiKey = "0195d879f1b5a8b4ff2978314e2109ca";
+    let url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
+
+    fetch(url)
+        .then(res => res.json())
+        .then(data => {
+            if (data.cod == 200) {
+                document.getElementById("weather").style.display = "block"; 
+                document.getElementById("weather").innerHTML = `
+                    <h2>🌍 Weather in ${data.name}, ${data.sys.country}</h2>
+                    <p><b>🌡️ Temperature:</b> ${data.main.temp}°C</p>
+                    <p><b>🥶 Feels Like:</b> ${data.main.feels_like}°C</p>
+                    <p><b>💦 Humidity:</b> ${data.main.humidity}%</p>
+                    <p><b>⛅ Condition:</b> ${data.weather[0].description}</p>
+                    <p><b>💨 Wind Speed:</b> ${data.wind.speed} m/s</p>
+                    
+                    <img src="https://openweathermap.org/img/wn/${data.weather[0].icon}@2x.png" alt="Weather Icon">
+                `;
+            } else {
+                document.getElementById("weather").style.display = "block";
+                document.getElementById("weather").innerHTML = "❌ City not found! Try again.";
+            }
+        })
+        
+}
